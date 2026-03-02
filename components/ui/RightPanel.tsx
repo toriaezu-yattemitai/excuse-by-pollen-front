eb10f271fd4e0cece588b063f2711cfb5f385d09
kmmz532 <mizuto1127no01@gmail.com>
2026-03-02 16:19:06 +0900

    feat: 右側パネルの言い訳表示部ともっと盛る、Xで共有するアクションボタンを実装

--- components/ui/RightPanel.tsx
+++ components/ui/RightPanel.tsx
@@ -1,10 +1,18 @@
+"use client";
+
+import ActionButtons from "./rightpanel/ActionButtons";
+import ResultCard from "./rightpanel/ResultCard";
+
 /**
  * 右側の生成後のパネルコンポーネント
  */
 export default function RightPanel() {
   return (
     <>
-
+      <div>
+        <ResultCard text="国家の損失を防ぐため、本日は休養を要します..." score={75} />
+        <ActionButtons />
+      </div>
     </>
   );
 }
---
+++ components/ui/rightpanel/ActionButtons.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "../common/Button";
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+
+/**
+ * もっと盛る、SNS共有ボタン
+ */
+export default function ActionButtons() {
+    const shareText = "花粉言い訳ジェネレーターで生成した結果をシェア！ #花粉エクスキューズジェネレーター";
+    const shareUrl = "https://excuse-by-pollen.vercel.app/";
+
+    const handleShare = () => {
+        const url = `https://x.com/intent/post?text=${encodeURIComponent(shareText)}&url=${encodeURIComponent(shareUrl)}`;
+        window.open(url, "_blank");
+    };
+
+    return (
+        <>
+            <div className={"flex flex-row gap-2"}>
+                <Button color="green" onClick={() => {
+                    console.log("もっと盛る");
+                }}>
+                    <FontAwesomeIcon icon={faArrowUp} />
+                    もっと盛る
+                </Button>
+                <Button onClick={handleShare}><FontAwesomeIcon icon={faXTwitter} />Xで共有する</Button>
+            </div>
+        </>
+    );
+}
