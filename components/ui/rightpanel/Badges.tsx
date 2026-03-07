c954538946b35393117114ba42937c371b92d3e4
kmmz532 <mizuto1127no01@gmail.com>
2026-03-07 14:53:23 +0900

    fix: カードと位置を揃える

--- components/ui/rightpanel/Badges.tsx
+++ components/ui/rightpanel/Badges.tsx
@@ -6,7 +6,7 @@ import Badge from "../common/Badge";
 export default function Badges({badges}: {badges: Record<string, string>}) {
 
     return (
-        <div className="pt-4 mt-4 flex gap-2 items-start flex-wrap">
+        <div className="mt-2 mx-2 flex gap-2 items-start flex-wrap">
             {badges && Object.entries(badges).map(([key, value]) => {
                 return (
                     <Badge key={key} color="yellow">{value}</Badge>
