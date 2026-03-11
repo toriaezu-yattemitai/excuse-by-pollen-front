c3ccb59981719a67c9851a7c654a2f8cf18d0940
kmmz532 <mizuto1127no01@gmail.com>
2026-03-11 14:12:39 +0900

    chore: 間違えてmainにcommitしたので取り消し

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/Header";
-import Footer from "@/components/ui/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +30,6 @@ export default function RootLayout({
       >
         <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
