e712c48ade2056169cd7a58d4c74954361556899
kmmz532 <mizuto1127no01@gmail.com>
2026-03-12 00:19:11 +0900

    fix: generateMetadata側でfetchする

--- app/share/page.tsx
+++ app/share/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import ShareContent from "./ShareContent";
+import fetchApi from "@/hooks/utils/APIUtil";
 
 export async function generateMetadata({ searchParams }: { searchParams: { id?: string, text?: string; score?: string } }): Promise<Metadata> {
   const params = await searchParams;
@@ -8,7 +9,20 @@ export async function generateMetadata({ searchParams }: { searchParams: { id?: 
 
   let ogImageUrl = '';
   if (params.id) {
-    ogImageUrl = `${baseUrl}/api/ogp-image?id=${params.id}`;
+    const response = await fetchApi("get-excuse/" + params.id, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const excuse = data.excuse;
+      const score = data.score;
+      ogImageUrl = `${baseUrl}/api/ogp-image?text=${encodeURIComponent(excuse)}&score=${score}`;
+    } else {
+      ogImageUrl = `${baseUrl}/api/ogp-image?id=${params.id}`;
+    }
+
   } else {
     const text = params.text || '何も生成されていないようです...';
     const score = params.score || '-1';
